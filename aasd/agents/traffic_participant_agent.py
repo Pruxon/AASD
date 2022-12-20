995f724833de27299d224be8820863f73ac98d6f
Pruxon <jasiekpr@gmail.com>
2022-12-20 22:54:12 +0100

    traffic_participant_agent in progress

--- aasd/agents/traffic_participant_agent.py
+++ aasd/agents/traffic_participant_agent.py
@@ -1,15 +1,55 @@
-# import spade as spade
-
-# class TrafficParticipantAgent (spade.agent.Agent):
-#     #Rola TrafficParticipant
-#     class SendAccidentInfoToManagerBehaviour(spade.behaviour.OneShotBehaviour):
-#         async def run(self) -> None:
-
-#     class DisplayAlertBehaviour(spade.behaviour.OneShotBehaviour):
-#         async def run(self) -> None:
-
-#     class PropagateEmergencyVehicleInfoBehaviour(spade.behaviour.OneShotBehaviour):
-#         async def run(self) -> None:
+import spade as spade
+import json as json
+from spade.template import Template
+from
 
 
-#     #Rola AccidentParticipant
+class TrafficParticipantAgent(spade.agent.Agent):
+
+    def __init__(self, vehicle, environment, *args, **kwargs):
+
+        super(self).__init__(*args, **kwargs)
+
+    def displayAlertBehaviour(self):
+        print("Jedzie Karetka!")
+
+    def propagateEmergencyVehicleInfoBehaviour(self, list):
+        # wez pobliskie pojazdy od środowiska
+        print("")
+
+    def createAccidentMessagebody(self, x: float, y: float) -> str:
+        dictionary = {'X': x, 'Y': y, 'message': "Accident happened, help needed"}
+        return json.dumps(dictionary)
+
+    def informManagerAfterAccident(self, x: float, y: float):
+        msg = spade.message.Message(to="manager@localhost")
+        msg.set_metadata("performative", "inform")
+        msg.set_metadata("msgType", "accidentInfo")
+        msg.body = self.createAccidentMessagebody(x, y)
+        await self.send(msg)
+
+    def accidentHappened(self):
+        self.informManagerAfterAccident(1, 2)
+        t1 = Template()
+        t1.set_metadata("msgType", "helpArrivalInfo")
+        self.add_behaviour(self.WaitForProcessedAccidentInfoBehaviour, template=t1)
+
+    class HandleForEmergencyVehicleIncomingBehaviour(spade.behaviour.CyclicBehaviour):
+        async def run(self):
+            msg = await self.receive()
+            if msg:
+                print("Emergency VehicleInfo received")
+
+    class WaitForProcessedAccidentInfoBehaviour(spade.behaviour.CyclicBehaviour):
+        async def run(self):
+            msg = await self.receive()
+            if msg:
+                print("Help is arriving in: " + msg.body)
+
+    # Rola AccidentParticipant
+
+    async def setup(self):
+        print(self.jid + " is starting")
+        t1 = Template()
+        t1.set_metadata("msgType", "evLocationData")
+        self.add_behaviour(self.HandleForEmergencyVehicleIncomingBehaviour, template=t1)
