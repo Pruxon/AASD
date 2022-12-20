c08f99a179e4fa3ed6a67766259a0d13b2bcc815
Pruxon <jasiekpr@gmail.com>
2022-12-20 20:06:00 +0100

    cokolwiek2

--- src/agents/manager_agent.py
+++ src/agents/manager_agent.py
@@ -3,11 +3,21 @@ import spade as spade
 class ManagerAgent(spade.agent.Agent):
     class ListenForAccidentsBehaviour(spade.behaviour.CyclicBehaviour):
         async def on_start(self) -> None:
-            print("Manager is starting to hear for accident info")
+            print("Manager is listening for accident info")
 
         async def run(self):
-            
+            msg = await self.receive(timeout=1)
 
+    class ListenForEmergencyVehicleInfoBehaviour(spade.behaviour.CyclicBehaviour):
+        async def on_start(self) -> None:
+            print("Manager is listening for emergency vehicle info")
+
+    class SendAccidentInfoProccessedBehaviour(spade.behaviour.OneShotBehaviour):
+        async def run(self) -> None:
+
+
+    class SendAccidentInfoToEmergencyVehicleBehaviour(spade.behaviour.OneShotBehaviour):
+        async def run(self) -> None:
 
 
 
--- src/agents/traffic_participant_agent.py
+++ src/agents/traffic_participant_agent.py
@@ -1,4 +1,16 @@
 import spade as spade
 
 class TrafficParticipantAgent (spade.agent.Agent):
-    class 
+    #Rola TrafficParticipant
+    class SendAccidentInfoToManagerBehaviour(spade.behaviour.OneShotBehaviour):
+        async def run(self) -> None:
+
+    class DisplayAlertBehaviour(spade.behaviour.OneShotBehaviour):
+        async def run(self) -> None:
+
+    class PropagateEmergencyVehicleInfoBehaviour(spade.behaviour.OneShotBehaviour):
+        async def run(self) -> None:
+
+
+
+    #Rola AccidentParticipant
