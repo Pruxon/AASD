8422d319248dbf506ccad633d0aaf15f49ff8980
Skwiwel <kuba.grzechocinski@gmail.com>
2022-12-20 21:11:30 +0100

    add dummy manager with behaviour templates

--- src/main.py
+++ src/main.py
@@ -1,14 +1,52 @@
-from spade import agent, quit_spade
+import time
+from spade.agent import Agent
+from spade.behaviour import OneShotBehaviour
+from spade.message import Message
+from spade.template import Template
+
+from agents.manager_agent import ManagerAgent
 
 
-class DummyAgent(agent.Agent):
+class SenderAgent(Agent):
+    class InformBehav(OneShotBehaviour):
+        async def run(self):
+            print("InformBehav running")
+            msg = Message(to="manager@localhost")
+            msg.set_metadata("performative", "inform")
+            msg.set_metadata("msgType", "accidentInfo")
+            msg.body = "Hello World"  # Set the message content
+
+            await self.send(msg)
+            print("Message sent!")
+
+            msg = Message(to="manager@localhost")
+            msg.set_metadata("performative", "inform")
+            msg.set_metadata("msgType", "evLocationData")
+            msg.body = "Hello World"  # Set the message content
+
+            await self.send(msg)
+            print("Message 2 sent!")
+
+            # stop agent from behaviour
+            await self.agent.stop()
+
     async def setup(self):
-        print("Hello World! I'm agent {}".format(str(self.jid)))
+        print("SenderAgent started")
+        b = self.InformBehav()
+        self.add_behaviour(b)
 
 
-dummy = DummyAgent("admin@localhost", "passw0rd")
-future = dummy.start()
-future.result()
+if __name__ == "__main__":
+    manager = ManagerAgent("manager@localhost", "passw0rd")
+    manager.start().wait()
+    sender = SenderAgent("admin@localhost", "passw0rd")
+    sender.start().wait()
 
-dummy.stop()
-quit_spade()
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            manager.stop()
+            sender.stop()
+            break
+    print("Agents finished")
