cb9946f255a44d37600cb6437640e658bb020bf5
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2022-12-21 23:38:49 +0100

    finish ev somewhat

--- aasd/__main__.py
+++ aasd/__main__.py
@@ -9,6 +9,7 @@ from spade.template import Template
 
 from aasd.agents.manager_agent import ManagerAgent
 from aasd.agents.traffic_participant_agent import TrafficParticipantAgent
+from aasd.agents.emergency_vehicle_agent import EmergencyVehicleAgent
 from aasd.gui import MovementSimulationWindow, get_color
 from aasd.environment import Environment
 from aasd.vehicle import Vehicle, VehicleType
@@ -51,27 +52,43 @@ if __name__ == "__main__":
     env = Environment()
 
     manager_user = UserToRegister("manager")
+    ev1_user = UserToRegister("ev1")
+    ev2_user = UserToRegister("ev2")
     tp1_user = UserToRegister("tp1")
 
-    register_users_to_ejabberd([manager_user, tp1_user])
+    register_users_to_ejabberd([manager_user, ev1_user, ev2_user, tp1_user])
 
     manager = ManagerAgent(manager_user.jid_str(), manager_user.password)
     manager.start().wait()
 
-    v1 = Vehicle(tp1_user.jid_str(), 500, 500, vehicle_type=VehicleType.Normal)
-    env.register_vehicle(v1)
+    ev1_v = Vehicle(ev1_user.jid_str(), 300, 700, vehicle_type=VehicleType.Emergency)
+    env.register_vehicle(ev1_v)
+    ev1 = EmergencyVehicleAgent(
+        vehicle=ev1_v, env=env, jid=ev1_user.jid_str(), password=ev1_user.password
+    )
+    ev1.start().wait()
+
+    ev2_v = Vehicle(ev2_user.jid_str(), 600, 800, vehicle_type=VehicleType.Emergency)
+    env.register_vehicle(ev2_v)
+    ev2 = EmergencyVehicleAgent(
+        vehicle=ev2_v, env=env, jid=ev2_user.jid_str(), password=ev2_user.password
+    )
+    ev2.start().wait()
+
+    tp1_v = Vehicle(tp1_user.jid_str(), 500, 500, vehicle_type=VehicleType.Normal)
+    env.register_vehicle(tp1_v)
     tp1 = TrafficParticipantAgent(
-        vehicle=v1, env=env, jid=tp1_user.jid_str(), password=tp1_user.password
+        vehicle=tp1_v, env=env, jid=tp1_user.jid_str(), password=tp1_user.password
     )
     tp1.start().wait()
 
-    vehicles = [
-        Vehicle("1", 200, 200, vehicle_type=VehicleType.Emergency),
-        Vehicle("2", 600, 600),
-        Vehicle("3", 400, 700, vehicle_type=VehicleType.Crashed),
-    ]
-    for vehicle in vehicles:
-        env.register_vehicle(vehicle)
+    # vehicles = [
+    #     Vehicle("1", 200, 200, vehicle_type=VehicleType.Emergency),
+    #     Vehicle("2", 600, 600),
+    #     Vehicle("3", 400, 700, vehicle_type=VehicleType.Crashed),
+    # ]
+    # for vehicle in vehicles:
+    #     env.register_vehicle(vehicle)
 
     simulation = MovementSimulationWindow(env)
 
--- aasd/agents/emergency_vehicle_agent.py
+++ aasd/agents/emergency_vehicle_agent.py
@@ -1,6 +1,7 @@
 import asyncio
 import spade as spade
 import json as json
+from math import dist
 from spade.template import Template
 from aasd.environment import Vehicle, Environment
 
@@ -13,6 +14,23 @@ class EmergencyVehicleAgent(spade.agent.Agent):
         self.accident_x = 0.0
         self.accident_y = 0.0
         super().__init__(*args, **kwargs)
+
+    def prune_behaviours(self):
+        for b in self.behaviours:
+            self.remove_behaviour(b)
+
+    def init_undispatched_behaviour(self):
+        print(f"{str(self.jid)}: switching to undispatched ev behaviour")
+        self.prune_behaviours()
+        t1 = Template()
+        t1.set_metadata("msgType", "processedAccidentInfo")
+        self.add_behaviour(self.UnassignedEvBehaviour(agent=self), template=t1)
+
+    def init_dispatched_behaviour(self):
+        print(f"{str(self.jid)}: switching to dispatched ev behaviour")
+        self.prune_behaviours()
+        self.add_behaviour(self.AlertOtherVehiclesBehaviour(agent=self))
+        self.add_behaviour(self.CheckIfArrivedBehaviour(agent=self))
 
     class UnassignedEvBehaviour(spade.behaviour.CyclicBehaviour):
         def __init__(self, agent, **kwargs):
@@ -29,10 +47,10 @@ class EmergencyVehicleAgent(spade.agent.Agent):
                 self.agent.accident_x = accident_info["x"]
                 self.agent.accident_y = accident_info["y"]
                 self.agent.is_assigned = True
-
                 print(
                     f"{str(self.agent.jid)}: dispatch message received to accident at x={self.agent.accident_x}, y={self.agent.accident_y}"
                 )
+                self.agent.init_dispatched_behaviour()
 
     class AlertOtherVehiclesBehaviour(spade.behaviour.CyclicBehaviour):
         def __init__(self, agent, **kwargs):
@@ -60,42 +78,20 @@ class EmergencyVehicleAgent(spade.agent.Agent):
             super().__init__(**kwargs)
 
         async def on_start(self) -> None:
-            acc_x, acc_y = self.agent.accident_x, self.agent.accident_y
-            v_x, v_y = self.agent.vehicle.get_coordinates()
-
-            self.agent.vehicle.change_direction()
+            self.agent.vehicle.change_direction_to_face_coordinates(
+                self.agent.accident_x, self.agent.accident_y
+            )
             print(
                 f"{str(self.agent.jid)} is starting to check if it arrived at the destination..."
             )
 
         async def run(self):
-            await asyncio.sleep(2)
-            nearby_vehicles = self.env.get_nearby_vehicles(self.vehicle, 10)
-            for vehicle in nearby_vehicles:
-                msg = spade.message.Message(to=vehicle.id)
-                msg.set_metadata("msgType", "emergencyVehicleInfo")
-                x, y = self.agent.vehicle.get_coordinates()
-                msg.body = json.dumps(
-                    {"x": x, "y": y, "direction": self.agent.vehicle.get_direction()}
-                )
-                await self.send(msg)
-
-    async def propagateEmergencyVehicleInfo(self, message: spade.message.Message):
-        nearby_vehicles = self.env.get_nearby_vehicles(self.vehicle, 5)
-        for vehicle in nearby_vehicles:
-            msg = spade.message.Message(to=vehicle.id)
-            msg.set_metadata("msgType", "emergencyVehicleInfo")
-            msg.body = message.body
-            await self.send(msg)
+            await asyncio.sleep(0.1)
+            x, y = self.agent.vehicle.get_coordinates()
+            if dist((x, y), (self.agent.accident_x, self.agent.accident_y)) < 1.0:
+                self.agent.is_assigned = True
+                self.agent.init_undispatched_behaviour()
 
     async def setup(self):
-        pass
-        # print(str(self.jid) + " is starting")
-        # t1 = Template()
-        # t1.set_metadata("msgType", "evLocationData")
-        # self.add_behaviour(
-        #     self.HandleForEmergencyVehicleIncomingBehaviour(agent=self), template=t1
-        # )
-        # t2 = Template()
-        # t2.set_metadata("msgType", "helpArrivalInfo")
-        # self.add_behaviour(self.HandleHelpArrivalInfoBehaviour(agent=self), template=t2)
+        print(f"{str(self.jid)} starting...")
+        self.init_undispatched_behaviour()
